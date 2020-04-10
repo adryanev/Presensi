b29902e9e72989bffc88764a9f946f27d06b3bb8
Adryan Eka Vandra <adryanekavandra@gmail.com>
2020-04-10 23:48:24 +0700

    added map view

--- app/src/main/java/com/adryanev/presensi/ui/lokasi/LokasiFragment.kt
+++ app/src/main/java/com/adryanev/presensi/ui/lokasi/LokasiFragment.kt
@@ -1,52 +1,69 @@
 package com.adryanev.presensi.ui.lokasi
 
-import android.Manifest
-import android.os.Build
-import androidx.lifecycle.ViewModelProviders
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
-import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
-import org.koin.androidx.viewmodel.ext.android.viewModel
-
-
+import androidx.fragment.app.Fragment
 import com.adryanev.presensi.R
 import com.adryanev.presensi.databinding.FragmentLokasiBinding
 import com.adryanev.presensi.utils.ToastHelper
 import com.adryanev.presensi.utils.toast
 import com.birjuvachhani.locus.Locus
-import com.karumi.dexter.Dexter
-import com.karumi.dexter.MultiplePermissionsReport
-import com.karumi.dexter.PermissionToken
-import com.karumi.dexter.listener.PermissionRequest
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class LokasiFragment : Fragment() {
+class LokasiFragment : Fragment(){
 
     val viewModel: LokasiViewModel by viewModel()
+    private lateinit var binding: FragmentLokasiBinding
+    companion object{
+        val MAPVIEW_BUNDLE_KEY = "MapViewBundleKey"
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentLokasiBinding.inflate(inflater, container, false)
+        binding = FragmentLokasiBinding.inflate(inflater, container, false)
+        var mapViewBundle: Bundle? = null
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY)
+        }
+        binding.mapView.onCreate(mapViewBundle)
         subscribeUi(binding)
 
         return binding.root
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        var mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY)
+        if (mapViewBundle == null) {
+            mapViewBundle = Bundle()
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle)
+        }
+
+        binding.mapView.onSaveInstanceState(mapViewBundle)
     }
 
 
 
     private fun subscribeUi(binding: FragmentLokasiBinding?) {
 
+        //start Map
+
+
        Locus.startLocationUpdates(this){ result ->
-           result.location?.let {
-               val lat = it.latitude.toString()
-               val long = it.longitude.toString()
+           result.location?.let {location ->
+               val lat = location.latitude.toString()
+               val long = location.longitude.toString()
                 binding?.latlong?.text = getString(R.string.latlong,lat,long)
+               binding?.mapView?.getMapAsync {
+                   it.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude,location.longitude),18f))
+                   it.addMarker(MarkerOptions().position(LatLng(location.latitude, location.longitude)).title(getString(R.string.lokasi_anda)))
+               }
 
                 requireContext().toast(ToastHelper.Info, "Current Location: lat=${lat} long=${long}")
            }
@@ -55,5 +72,37 @@ class LokasiFragment : Fragment() {
            }
        }
     }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onPause() {
+        binding.mapView.onPause();
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        binding.mapView.onLowMemory()
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+
 
 }
